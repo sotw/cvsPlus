ec045985dbc460a587e1f8aefc2607dadbcbb45c
Hammer <workthethird@gmail.com>
2014-05-15 09:54:18 +0800

    fix broken script in perfrom.sh

--- perform.sh
+++ perform.sh
@@ -7,7 +7,7 @@ tFolder="mx4j"
 CMD="cvs -z3 -d:pserver:anonymous@mx4j.cvs.sourceforge.net:/cvsroot/mx4j co -P mx4j"
 tSrv="pserver:anonymous@mx4j.cvs.sourceforge.net:/cvsroot/mx4j"
 else
-CMD="cvs -z3 -d:"$1" co -P" $2
+CMD="cvs -z3 -d:"$1" co -P "$2""
 tFolder=$2
 tSrv=$1
 fi
